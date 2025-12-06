2b168bb11133143e78c406a8030b2574fa081cc7
cylcoder <cylcoder@gmail.com>
2025-12-07 00:27:47 +0900

    chore: 프로젝트 초기 세팅 및 필수 의존성 추가

---
+++ src/test/java/com/wingman/WingmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wingman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WingmanApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
