42993c3cb676f0de470c931d7b4589aeb614ce74
cylcoder <cylcoder@gmail.com>
2025-12-07 14:58:57 +0900

    fix: @EnableJpaAuditing 추가

--- src/main/java/com/wingman/WingmanApplication.java
+++ src/main/java/com/wingman/WingmanApplication.java
@@ -2,8 +2,10 @@ package com.wingman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WingmanApplication {
 
   public static void main(String[] args) {
